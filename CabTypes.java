249e0bfcb2d46edd953661aa4f5c1cd45c116903
MuthuKumari-P <82449715+MuthuKumari-P@users.noreply.github.com>
2021-04-16 00:13:41 +0530

    Update CabTypes.java

--- CabTypes.java
+++ CabTypes.java
@@ -2,15 +2,8 @@ package com.muthukumarip;
 
 public class CabTypes {
 	public static void cab() {
-		System.out.println(
-				"We are have 3 types of cabs\n1. MICRO (Rs.10/km)\n2. MINI (Rs.15/km)\n3. PRIME (Rs.20/km)");// Shows
-																												// the
-																												// types
-																												// of
-																												// cars
-																												// available
-																												// in
-																												// here
+		System.out.println("We are have 3 types of cabs\n1. MICRO (Rs.10/km)\n2. MINI (Rs.15/km)\n3. PRIME (Rs.20/km)");// Shows the types of cars available in here.
+															       																									// here
 		PriceEstimator.cabType();//call PriceEstimator class with cabType method
 	}
 
